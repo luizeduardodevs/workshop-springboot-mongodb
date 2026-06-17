0d0fdd78ba236e7fa81026bcbe1e357c82e7e2f1
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-06-17 01:13:48 -0300

    Entity User and Rest

---
+++ src/main/java/com/luizeduardo/workshopmongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.luizeduardo.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luizeduardo.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	
+	//@RequestMapping(method=RequestMethod.GET)
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {//responseentity encapsula toda uma estrutura necessaria pra retornar uma reposta http
+		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
+		User alex = new User("1002", "Alex Green", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria,alex));
+		return ResponseEntity.ok().body(list);//.ok instancia o response entity ja com o codigo de reposta
+		//.body quer o corpo da resposta
+	}
+}
