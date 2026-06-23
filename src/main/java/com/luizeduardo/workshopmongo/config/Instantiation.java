ad694ef3a2fb8817ea6aefbf42cc1cbedf36d796
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-06-23 01:06:45 -0300

    reference of posts no user

--- src/main/java/com/luizeduardo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luizeduardo/workshopmongo/config/Instantiation.java
@@ -41,6 +41,8 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("21/03/2022"),"Let´s go travel","I´m Traveling from São Paulo",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("22/03/2022"),"Good morning","I woke up very happy", new AuthorDTO(maria)); 
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 		
 		
 	}
--- src/main/java/com/luizeduardo/workshopmongo/domain/User.java
+++ src/main/java/com/luizeduardo/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.luizeduardo.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 //pra falar que corresponde a uma colleção do mongo db, com collection voce coloca o nome que quiser
@@ -15,7 +18,18 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy=true) //é usado para referenciar os posts, cada post vai ter sua referencia
+	//lazy=true so acessa a lista quando abrir os posts no banco de dados 
+	private List<Post> posts = new ArrayList<>();
 	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	public User() {}
 
 	public User(String id, String name, String email) {
