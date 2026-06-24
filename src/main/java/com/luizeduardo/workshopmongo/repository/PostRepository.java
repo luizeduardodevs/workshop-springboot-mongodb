b5f404584f620fdfc9f95af3a165312641caf5ca
Luiz Eduardo <luizeduardoledsm@gmail.com>
2026-06-23 23:44:14 -0300

    consult with more arguments

--- src/main/java/com/luizeduardo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luizeduardo/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.luizeduardo.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,7 +15,10 @@ public interface PostRepository extends MongoRepository<Post, String> {//user é
 	//@Query("{ <field>: { $regex: /pattern/, $options: '<options>' } }")
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
+	//os dois sao para a mesma coisa coisa
 	List <Post> findByTitleContainingIgnoreCase(String text);//ignorecase e feito pro case sensitive
 
+	//consulta de string por entre datas
+	@Query("{ $and: [ {date: { $gte: ?1}}, {date: { $lte: ?2 }} , { $or: [ { 'title': { $regex: ?0, $options: 'i'  }}, { 'body': { $regex: ?0, $options: 'i' }},{ 'comments.text': { $regex: ?0, $options: 'i'}} ] } ] }")
+	List<Post> fullSearch(String text,Date minDate, Date maxDate);
 }
